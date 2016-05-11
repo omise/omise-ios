6d2dca0c80482cf63e736e8ef10e628ae3154ea6
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-11 16:50:54 +0700

    add card model

---
+++ OmiseSDK/Tokenizer/Omise.swift
@@ -0,0 +1,14 @@
+//
+//  Omise.swift
+//  OmiseSDK
+//
+//  Created by Anak Mirasing on 5/11/16.
+//  Copyright © 2016 Omise. All rights reserved.
+//
+
+import Foundation
+
+public class Omise: NSObject {
+    
+    
+}
---
+++ OmiseSDK/Tokenizer/OmiseCard.swift
@@ -0,0 +1,29 @@
+//
+//  OmiseCard.swift
+//  OmiseSDK
+//
+//  Created by Anak Mirasing on 5/11/16.
+//  Copyright © 2016 Omise. All rights reserved.
+//
+
+public class OmiseCard {
+    
+    var cardId: String?
+    var livemode: Bool?
+    var location: String?
+    var country: String?
+    var number: String?
+    var city: String?
+    var postalCode: String?
+    var financing: String?
+    var lastDigits: String?
+    var brand: String?
+    var expirationMonth: String?
+    var expirationYear: String?
+    var fingerprint: String?
+    var name: String?
+    var created: String?
+    var securityCode: String?
+    var securityCodeCheck: Bool?
+    var bank: String?
+}
