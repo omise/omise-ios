af2f38e402a9b4a70f8fcf385ab361d9306d571d
Chakrit Wichian <service@chakrit.net>
2016-05-11 15:11:06 +0700

    Project init.

---
+++ OmiseSDK/OmiseSDK.h
@@ -0,0 +1,19 @@
+//
+//  OmiseSDK.h
+//  OmiseSDK
+//
+//  Created by Chakrit Wichian on 5/11/16.
+//  Copyright © 2016 Omise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OmiseSDK.
+FOUNDATION_EXPORT double OmiseSDKVersionNumber;
+
+//! Project version string for OmiseSDK.
+FOUNDATION_EXPORT const unsigned char OmiseSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OmiseSDK/PublicHeader.h>
+
+
