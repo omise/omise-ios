673af8b5ddd2bd64d228d0f560e32dfb94c5d653
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-12 16:01:35 +0700

    set public access to parse omise token method

--- OmiseSDK/Tokenizer/OmiseJsonParser.swift
+++ OmiseSDK/Tokenizer/OmiseJsonParser.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public class OmiseJsonParser: NSObject {
     
-    func parseOmiseToken(json: NSString) -> OmiseToken? {
+    public func parseOmiseToken(json: NSString) -> OmiseToken? {
         
         var jsonObject: AnyObject?
         
