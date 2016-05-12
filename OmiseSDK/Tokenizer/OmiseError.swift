0aa818816910e66e15bb236fd6226824eee2b3a6
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-12 16:43:43 +0700

    add omise error type

---
+++ OmiseSDK/Tokenizer/OmiseError.swift
@@ -0,0 +1,13 @@
+//
+//  OmiseError.swift
+//  OmiseSDK
+//
+//  Created by Anak Mirasing on 5/12/16.
+//  Copyright © 2016 Omise. All rights reserved.
+//
+
+import Foundation
+
+public enum OmiseError: ErrorType {
+    
+}
