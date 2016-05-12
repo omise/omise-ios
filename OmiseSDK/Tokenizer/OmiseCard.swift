5153631ec42a33a8abaafd5d2bb4d39e8552f9d0
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-12 15:59:49 +0700

    set public access to omise card properties

--- OmiseSDK/Tokenizer/OmiseCard.swift
+++ OmiseSDK/Tokenizer/OmiseCard.swift
@@ -8,23 +8,23 @@
 
 public class OmiseCard {
     
-    var cardId: String?
-    var livemode: Bool?
-    var location: String?
-    var country: String?
-    var financing: String?
-    var lastDigits: String?
-    var brand: String?
-    var fingerprint: String?
-    var securityCodeCheck: Bool?
-    var created: String?
+    public var cardId: String?
+    public var livemode: Bool?
+    public var location: String?
+    public var country: String?
+    public var financing: String?
+    public var lastDigits: String?
+    public var brand: String?
+    public var fingerprint: String?
+    public var securityCodeCheck: Bool?
+    public var created: String?
     
     // For create a token
-    var name: String?
-    var number: String?
-    var expirationMonth: Int?
-    var expirationYear: Int?
-    var securityCode: String?
-    var city: String?           // Optional
-    var postalCode: String?     // Optional
+    public var name: String?
+    public var number: String?
+    public var expirationMonth: Int?
+    public var expirationYear: Int?
+    public var securityCode: String?
+    public var city: String?           // Optional
+    public var postalCode: String?     // Optional
 }
