0e58bdd4152de6b10912c4afe649ae627f6fac99
Anak Mirasing <thaihooligan@hotmail.com>
2016-05-13 12:38:39 +0700

    add error code and class function for return nserror from api response

--- OmiseSDK/Tokenizer/OmiseError.swift
+++ OmiseSDK/Tokenizer/OmiseError.swift
@@ -8,9 +8,46 @@
 
 import Foundation
 
+let OmiseErrorDomain = "co.omise"
+
+public enum OmiseErrorCode: Int {
+    case InvalidRequestError = 50
+    case APIError = 60
+    case UnexpectedError = 70
+}
+
 public class OmiseError {
     
     public var location: String?
     public var code: String?
     public var message: String?
+    
+    class func errorFromResponse(error: OmiseError) -> NSError {
+        
+        guard let code = error.code, let message = error.message else {
+            
+            let userInfo: [NSObject: AnyObject] =
+                [ NSLocalizedDescriptionKey :  NSLocalizedString("UnexpectedError", value: "Unexpected error", comment: "") ]
+            
+            return NSError(domain: OmiseErrorDomain, code: OmiseErrorCode.UnexpectedError.rawValue, userInfo: userInfo)
+        }
+        
+        var errorCode = 0
+        
+        let userInfo: [NSObject: AnyObject] =
+            [ NSLocalizedDescriptionKey:  NSLocalizedString(code, value: message, comment: "") ]
+        
+        switch code {
+        case "authentication_failure", "invalid_card":
+            errorCode = OmiseErrorCode.InvalidRequestError.rawValue
+            break
+        default:
+            errorCode = OmiseErrorCode.APIError.rawValue
+            break
+        }
+        
+        return NSError(domain: OmiseErrorDomain, code: errorCode, userInfo: userInfo)
+        
+    }
+    
 }
