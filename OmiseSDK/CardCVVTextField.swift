53af9810179cf24f52cfeb4637afb0caaa68ce6d
Chakrit Wichian <service@chakrit.net>
2016-06-21 14:52:37 +0700

    Remove UITextFieldDelegate reference in CVV field.

--- OmiseSDK/CardCVVTextField.swift
+++ OmiseSDK/CardCVVTextField.swift
@@ -1,7 +1,7 @@
 import Foundation
 import UIKit
 
-public class CardCVVTextField: OmiseTextField, UITextFieldDelegate {
+public class CardCVVTextField: OmiseTextField {
     private let maxLength = 3
     
     public override var isValid: Bool {
@@ -27,20 +27,5 @@ public class CardCVVTextField: OmiseTextField, UITextFieldDelegate {
         keyboardType = .NumberPad
         placeholder = "123"
         secureTextEntry = true
-        delegate = self // TODO: 👈 remove, replace with mask instead
-    }
-   
-    public func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-        if string.characters.count == 0 && range.length == 1 {
-            if range.location == maxLength {
-                deleteBackward()
-            }
-        }
-        
-        if(range.length + range.location > maxLength) {
-            return false
-        }
-        
-        return true
     }
 }
