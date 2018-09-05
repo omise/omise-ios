dca93978f477c5ebccee078c31397c49fbb61bec
Pitiphong Phongpattranont <pitiphong.p@me.com>
2018-09-05 15:05:11 +0700

    Added: Form text fields accessory view for EContext screen

--- OmiseSDK/EContextInformationInputViewController.swift
+++ OmiseSDK/EContextInformationInputViewController.swift
@@ -13,13 +13,27 @@ class EContextInformationInputViewController: UIViewController, PaymentSourceCre
     @IBOutlet var submitButton: MainActionButton!
     @IBOutlet var requestingIndicatorView: UIActivityIndicatorView!
     
+    @IBOutlet var formFields: [OmiseTextField]!
+    @IBOutlet var formFieldsAccessoryView: UIToolbar!
+    @IBOutlet var gotoPreviousFieldBarButtonItem: UIBarButtonItem!
+    @IBOutlet var gotoNextFieldBarButtonItem: UIBarButtonItem!
+    @IBOutlet var doneEditingBarButtonItem: UIBarButtonItem!
+    
+    private var currentEditingTextField: OmiseTextField?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        
+        formFields.forEach({
+            $0.inputAccessoryView = formFieldsAccessoryView
+        })
     }
     
-    @IBAction func submitEContextForm(_ sender: UIButton) {
+    @IBAction func submitEContextForm(_ sender: AnyObject) {
         guard let fullname = fullNameTextField.text, let email = emailTextField.text,
             let phoneNumber = phoneNumberTextField.text else {
                 return
@@ -29,14 +43,45 @@ class EContextInformationInputViewController: UIViewController, PaymentSourceCre
         requestingIndicatorView.startAnimating()
         view.isUserInteractionEnabled = false
         view.tintAdjustmentMode = .dimmed
-        sender.isEnabled = false
+        submitButton.isEnabled = false
         requestCreateSource(PaymentInformation.eContext(eContextInformation), completionHandler: { _ in
             self.requestingIndicatorView.stopAnimating()
             self.view.isUserInteractionEnabled = true
             self.view.tintAdjustmentMode = .automatic
-            sender.isEnabled = true
+            self.submitButton.isEnabled = true
         })
     }
     
+    @IBAction func updateInputAccessoryViewFor(_ sender: OmiseTextField) {
+        guard formFields.contains(sender) else { return }
+        
+        currentEditingTextField = sender
+        gotoPreviousFieldBarButtonItem.isEnabled = sender !== formFields.first
+        gotoNextFieldBarButtonItem.isEnabled = sender !== formFields.last
+    }
+    
+    @objc @IBAction private func gotoPreviousField(_ button: UIBarButtonItem) {
+        guard let currentTextField = currentEditingTextField, let index = formFields.index(of: currentTextField) else {
+            return
+        }
+        
+        let prevIndex = index - 1
+        guard prevIndex >= 0 else { return }
+        formFields[prevIndex].becomeFirstResponder()
+    }
+    
+    @objc @IBAction private func gotoNextField(_ sender: AnyObject) {
+        guard let currentTextField = currentEditingTextField, let index = formFields.index(of: currentTextField) else {
+            return
+        }
+        
+        let nextIndex = index + 1
+        guard nextIndex < formFields.count else { return }
+        formFields[nextIndex].becomeFirstResponder()
+    }
+    
+    @objc @IBAction private func doneEditing(_ button: UIBarButtonItem?) {
+        view.endEditing(true)
+    }
 }
 
