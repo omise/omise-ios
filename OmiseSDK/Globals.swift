6406976280bb3a185c993516c57fc5b98a52a8fd
Pitiphong Phongpattranont <pitiphong.p@me.com>
2018-10-25 15:44:17 +0700

    Revised a few EN strings

--- OmiseSDK/Globals.swift
+++ OmiseSDK/Globals.swift
@@ -473,7 +473,7 @@ extension OmiseError {
                         let preferredErrorDescriptionFormat = NSLocalizedString(
                             "payment-creator.error.api.bad_request.amount-is-less-than-valid-amount.with-valid-amount.recovery-suggestion",
                             tableName: "Error", bundle: Bundle.omiseSDKBundle,
-                            value: "The payment amount is too low. Please contact the merchant",
+                            value: "The payment amount is too high. Please make a payment with a lower amount",
                             comment: "The displaying recovery from error suggestion showing in the error banner in the built-in Payment Creator an `Bad request` error with `amount-is-less-than-valid-amount.with-valid-amount` from the backend has occurred"
                         )
                         let formatter = NumberFormatter.makeAmountFormatter(for: currency)
@@ -482,7 +482,7 @@ extension OmiseError {
                         return NSLocalizedString(
                             "payment-creator.error.api.bad_request.amount-is-less-than-valid-amount.without-valid-amount.recovery-suggestion",
                             tableName: "Error", bundle: Bundle.omiseSDKBundle,
-                            value: "The payment amount is too low. Please contact the merchant",
+                            value: "The payment amount is too high. Please make a payment with a lower amount",
                             comment: "The displaying recovery from error suggestion showing in the error banner in the built-in Payment Creator an `Bad request` error with `amount-is-less-than-valid-amount.without-valid-amount` from the backend has occurred"
                         )
                     }
@@ -491,7 +491,7 @@ extension OmiseError {
                         let preferredErrorDescriptionFormat = NSLocalizedString(
                             "payment-creator.error.api.bad_request.amount-is-greater-than-valid-amount.with-valid-amount.recovery-suggestion",
                             tableName: "Error", bundle: Bundle.omiseSDKBundle,
-                            value: "The payment amount is too high. Please contact the merchant.",
+                            value: "The payment amount is too high. Please make a payment with a higher amount.",
                             comment: "The displaying recovery from error suggestion showing in the error banner in the built-in Payment Creator an `Bad request` error with `amount-is-greater-than-valid-amount.with-valid-amount` from the backend has occurred"
                         )
                         let formatter = NumberFormatter.makeAmountFormatter(for: currency)
@@ -500,7 +500,7 @@ extension OmiseError {
                         return NSLocalizedString(
                             "payment-creator.error.api.bad_request.amount-is-greater-than-valid-amount.without-valid-amount.recovery-suggestion",
                             tableName: "Error", bundle: Bundle.omiseSDKBundle,
-                            value: "The payment amount is too high. Please contact the merchant.",
+                            value: "The payment amount is too high. Please make a payment with a higher amount.",
                             comment: "The displaying recovery from error suggestion showing in the error banner in the built-in Payment Creator an `Bad request` error with `amount-is-greater-than-valid-amount.without-valid-amount` from the backend has occurred"
                         )
                     }
