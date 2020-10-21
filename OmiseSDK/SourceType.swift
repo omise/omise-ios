752d2512ad08fa6795fa7d35e8bd001cd5e48ec4
Anak Mirasing <anak@omise.co>
2020-10-21 21:00:14 +0700

    Revise SourceType enum and add necessary extensions

--- OmiseSDK/SourceType.swift
+++ OmiseSDK/SourceType.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-enum SourceType {
+public enum SourceType {
     case internetBankingBAY
     case internetBankingKTB
     case internetBankingSCB
@@ -17,10 +17,12 @@ enum SourceType {
     case barcodeAlipay
     case billPaymentTescoLotus
     case econtext
-    case promptPay
+    case promptpay
     case paynow
     case truemoney
     case pointsCiti
+    
+    case other(value: String)
     
     var value: String {
         switch self {
@@ -54,7 +56,7 @@ enum SourceType {
             return "bill_payment_tesco_lotus"
         case .econtext:
             return "econtext"
-        case .promptPay:
+        case .promptpay:
             return "promptpay"
         case .paynow:
             return "paynow"
@@ -62,6 +64,8 @@ enum SourceType {
             return "truemoney"
         case .pointsCiti:
             return "points_citi"
+        case .other(let value):
+            return value
         }
     }
     
@@ -79,7 +83,7 @@ enum SourceType {
             return "bill_payment"
         case .econtext:
             return "econtext"
-        case .promptPay:
+        case .promptpay:
             return "promptpay"
         case .paynow:
             return "paynow"
@@ -87,6 +91,111 @@ enum SourceType {
             return "truemoney"
         case .pointsCiti:
             return "points"
+        case .other:
+            return ""
+        }
+    }
+    
+    init(_ value: String) {
+        switch value {
+        case SourceType.internetBankingBAY.value:
+            self = .internetBankingBAY
+        case SourceType.internetBankingKTB.value:
+            self = .internetBankingKTB
+        case SourceType.internetBankingSCB.value:
+            self = .internetBankingSCB
+        case SourceType.internetBankingBBL.value:
+            self = .internetBankingBBL
+            
+        case SourceType.installmentBAY.value:
+            self = .installmentBAY
+        case SourceType.installmentFirstChoice.value:
+            self = .installmentFirstChoice
+        case SourceType.installmentBBL.value:
+            self = .installmentBBL
+        case SourceType.installmentKTC.value:
+            self = .installmentKTC
+        case SourceType.installmentKBank.value:
+            self = .installmentKBank
+        case SourceType.installmentSCB.value:
+            self = .installmentSCB
+            
+        case SourceType.alipay.value:
+            self = .alipay
+        case SourceType.barcodeAlipay.value:
+            self = .barcodeAlipay
+        case SourceType.billPaymentTescoLotus.value:
+            self = .billPaymentTescoLotus
+        case SourceType.econtext.value:
+            self = .econtext
+        case SourceType.promptpay.value:
+            self = .promptpay
+        case SourceType.paynow.value:
+            self = .paynow
+        case SourceType.truemoney.value:
+            self = .truemoney
+        case SourceType.pointsCiti.value:
+            self = .pointsCiti
+            
+        default:
+            self = .other(value: value)
+        }
+    }
+}
+
+extension SourceType: Hashable { }
+
+extension SourceType {
+    var installmentBrand: PaymentInformation.Installment.Brand? {
+        switch self {
+        case .installmentBAY:
+            return .bay
+        case .installmentFirstChoice:
+            return .firstChoice
+        case .installmentBBL:
+            return .bbl
+        case .installmentKTC:
+            return .ktc
+        case .installmentKBank:
+            return .kBank
+        case .installmentSCB:
+            return .scb
+        default:
+            return nil
+        }
+    }
+    
+    var isInstallmentSource: Bool {
+        switch self {
+        case .installmentBAY, .installmentFirstChoice, .installmentBBL, .installmentKTC, .installmentKBank, .installmentSCB:
+            return true
+        default:
+            return false
+        }
+        
+    }
+    
+    var internetBankingSource: PaymentInformation.InternetBanking? {
+        switch self {
+        case .internetBankingBAY:
+            return .bay
+        case .internetBankingKTB:
+            return .ktb
+        case .internetBankingSCB:
+            return .scb
+        case .internetBankingBBL:
+            return .bbl
+        default:
+            return nil
+        }
+    }
+    
+    var isInternetBankingSource: Bool {
+        switch self {
+        case .internetBankingBAY, .internetBankingKTB, .internetBankingSCB, .internetBankingBBL:
+            return true
+        default:
+            return false
         }
     }
 }
