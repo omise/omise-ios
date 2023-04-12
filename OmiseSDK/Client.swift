c30b2c1cb0609f4c2cbdc5b72d0fb591feb81877
Andrei Solovev <andrei@opn.ooo>
2023-04-12 08:52:46 +0700

    Fix SonarCloud issue 'Define a constant instead of duplicating' in Client.swift

--- OmiseSDK/Client.swift
+++ OmiseSDK/Client.swift
@@ -1,13 +1,24 @@
 import UIKit
 import os
 
+// swiftlint:disable file_length
 @objc(OMSSDKClient) public class Client: NSObject {
     let session: URLSession
     let queue: OperationQueue
     let publicKey: String
     
     var userAgent: String?
-    
+
+    enum HTTPHeaders: String {
+        case authorization = "Authorization"
+        case userAgent = "User-Agent"
+        case contentType = "Content-Type"
+        case omiseVersion = "Omise-Version"
+    }
+
+    struct HTTPHeaderss {
+        let authorization = "Authorization"
+    }
     /// Initializes a new Client with the given Public Key and Operating OperationQueue
     ///
     /// - Parameters:
@@ -258,30 +269,30 @@ extension Client {
         urlRequest.httpMethod = "POST"
         let encoder = Client.makeJSONEncoder()
         urlRequest.httpBody = try? encoder.encode(request.parameter)
-        urlRequest.setValue(Client.encodeAuthorizationHeader(publicKey), forHTTPHeaderField: "Authorization")
-        urlRequest.setValue(userAgent ?? Client.defaultUserAgent, forHTTPHeaderField: "User-Agent")
-        urlRequest.setValue(Client.omiseAPIContentType, forHTTPHeaderField: "Content-Type")
-        urlRequest.setValue(Client.omiseAPIVersion, forHTTPHeaderField: "Omise-Version")
+        urlRequest.setValue(Client.encodeAuthorizationHeader(publicKey), forHTTPHeaderField: HTTPHeaders.authorization.rawValue)
+        urlRequest.setValue(userAgent ?? Client.defaultUserAgent, forHTTPHeaderField: HTTPHeaders.userAgent.rawValue)
+        urlRequest.setValue(Client.omiseAPIContentType, forHTTPHeaderField: HTTPHeaders.contentType.rawValue)
+        urlRequest.setValue(Client.omiseAPIVersion, forHTTPHeaderField: HTTPHeaders.omiseVersion.rawValue)
         return urlRequest
     }
     
     private func buildCapabilityAPIURLRequest() -> URLRequest {
         var urlRequest = URLRequest(url: Configuration.default.environment.capabilityURL)
         urlRequest.httpMethod = "GET"
-        urlRequest.setValue(Client.encodeAuthorizationHeader(publicKey), forHTTPHeaderField: "Authorization")
-        urlRequest.setValue(userAgent ?? Client.defaultUserAgent, forHTTPHeaderField: "User-Agent")
-        urlRequest.setValue(Client.omiseAPIContentType, forHTTPHeaderField: "Content-Type")
-        urlRequest.setValue(Client.omiseAPIVersion, forHTTPHeaderField: "Omise-Version")
+        urlRequest.setValue(Client.encodeAuthorizationHeader(publicKey), forHTTPHeaderField: HTTPHeaders.authorization.rawValue)
+        urlRequest.setValue(userAgent ?? Client.defaultUserAgent, forHTTPHeaderField: HTTPHeaders.userAgent.rawValue)
+        urlRequest.setValue(Client.omiseAPIContentType, forHTTPHeaderField: HTTPHeaders.contentType.rawValue)
+        urlRequest.setValue(Client.omiseAPIVersion, forHTTPHeaderField: HTTPHeaders.omiseVersion.rawValue)
         return urlRequest
     }
 
     private func buildRetrieveTokenURLRequest(from tokenID: String) -> URLRequest {
         var urlRequest = URLRequest(url: Configuration.default.environment.tokenURL.appendingPathComponent(tokenID))
         urlRequest.httpMethod = "GET"
-        urlRequest.setValue(Client.encodeAuthorizationHeader(publicKey), forHTTPHeaderField: "Authorization")
-        urlRequest.setValue(userAgent ?? Client.defaultUserAgent, forHTTPHeaderField: "User-Agent")
-        urlRequest.setValue(Client.omiseAPIContentType, forHTTPHeaderField: "Content-Type")
-        urlRequest.setValue(Client.omiseAPIVersion, forHTTPHeaderField: "Omise-Version")
+        urlRequest.setValue(Client.encodeAuthorizationHeader(publicKey), forHTTPHeaderField: HTTPHeaders.authorization.rawValue)
+        urlRequest.setValue(userAgent ?? Client.defaultUserAgent, forHTTPHeaderField: HTTPHeaders.userAgent.rawValue)
+        urlRequest.setValue(Client.omiseAPIContentType, forHTTPHeaderField: HTTPHeaders.contentType.rawValue)
+        urlRequest.setValue(Client.omiseAPIVersion, forHTTPHeaderField: HTTPHeaders.omiseVersion.rawValue)
         return urlRequest
     }
     
