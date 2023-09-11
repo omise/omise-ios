a806056e8ff55bc28bbfc3b8c98459312130ae5a
Andrei Solovev <andrei@opn.ooo>
2023-09-12 00:42:59 +0700

    Add UICustomization classes and convenience initializers for Netcetera UI customization

---
+++ OmiseSDK/3DS/UICustomization/UILabelCustomization.swift
@@ -0,0 +1,9 @@
+//
+//  LabelCustomization.swift
+//  OmiseSDK
+//
+//  Created by Andrei Solovev on 11/9/23.
+//  Copyright © 2023 Omise. All rights reserved.
+//
+
+import Foundation
---
+++ OmiseSDK/3DS/UICustomization/UITextBoxCustomization.swift
@@ -0,0 +1,9 @@
+//
+//  TextBoxCustomization.swift
+//  OmiseSDK
+//
+//  Created by Andrei Solovev on 11/9/23.
+//  Copyright © 2023 Omise. All rights reserved.
+//
+
+import Foundation
