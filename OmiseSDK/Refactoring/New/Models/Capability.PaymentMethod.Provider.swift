e59f34887fca575f3e6c383fd7c6bf4290cd5ead
Andrei Solovev <andrei@opn.ooo>
2024-02-13 17:52:04 +0700

    Capability tests added

--- OmiseSDK/Refactoring/New/Models/Capability.PaymentMethod.Provider.swift
+++ OmiseSDK/Refactoring/New/Models/Capability.PaymentMethod.Provider.swift
@@ -3,12 +3,12 @@ import Foundation
 extension Capability.PaymentMethod {
     // TODO: Add Unit Tests to Provider
     // TODO: Add comments to Provider's properties
-    enum Provider: String, Codable, Hashable {
+    public enum Provider: String, Codable, Hashable {
         case alipayPlus = "Alipay_plus"
         case rms = "RMS"
         case unknown
 
-        init(from decoder: Decoder) throws {
+        public init(from decoder: Decoder) throws {
             self = try Self(rawValue: decoder.singleValueContainer().decode(RawValue.self)) ?? .unknown
         }
     }
--- OmiseSDK/Refactoring/New/Models/Capability.PaymentMethod.swift
+++ OmiseSDK/Refactoring/New/Models/Capability.PaymentMethod.swift
@@ -3,13 +3,22 @@ import Foundation
 extension Capability {
     // TODO: Add Unit Tests to PaymentMethod
     // TODO: Add comments to PaymentMethod's properties
-    public struct PaymentMethod: Codable {
+    public struct PaymentMethod: Codable, Equatable {
         public let name: String
         public let currencies: Set<String>
         public let cardBrands: Set<String>?
         public let installmentTerms: Set<Int>?
         public let banks: Set<Bank>?
         private let provider: Provider?
+
+        public init(name: String, currencies: Set<String>, cardBrands: Set<String>?, installmentTerms: Set<Int>?, banks: Set<Bank>?, provider: Provider?) {
+            self.name = name
+            self.currencies = currencies
+            self.cardBrands = cardBrands
+            self.installmentTerms = installmentTerms
+            self.banks = banks
+            self.provider = provider
+        }
 
         private enum CodingKeys: String, CodingKey {
             case name
