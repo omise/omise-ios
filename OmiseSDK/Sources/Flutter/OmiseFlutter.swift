9bf18ca5333e2b2ff9c60bb569874f3138136c67
tyler-techcomebine <saihanlinaung@techcombine.co>
2025-02-11 11:30:57 +0700

    [MIT-3119] add flutter integration files

---
+++ OmiseSDK/Sources/Flutter/OmiseFlutter.swift
@@ -0,0 +1,14 @@
+//
+//  OmiseFlutter.swift
+//  dev
+//
+//  Created by Tyler on 5/2/2568 BE.
+//  Copyright © 2568 BE Omise. All rights reserved.
+//
+
+enum OmiseFlutter {
+    static let engineName: String = "OmiseFlutter"
+    static let channelName: String = "omiseFlutterChannel"
+    static let selectPaymentMethodName: String = "selectPaymentMethod"
+    static let selectPaymentMethodResult: String = "selectPaymentMethodResult"
+}
