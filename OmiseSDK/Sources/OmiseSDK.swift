c76506160e56b0f3d5aaaf7e324c21861f8e3daf
tyler-techcombine <saihanlinaung@techcombine.co>
2025-02-13 14:43:18 +0700

    replace payment method native screen with flutter

--- OmiseSDK/Sources/OmiseSDK.swift
+++ OmiseSDK/Sources/OmiseSDK.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import Flutter
 
 public class OmiseSDK {
     /// Static container that allows to assign a shared instance of OmiseSDK to be used as a Singleton object
@@ -29,6 +30,7 @@ public class OmiseSDK {
     private var expectedReturnURLStrings: [String] = []
     private var netceteraThreeDSController: NetceteraThreeDSController?
 
+    private let flutterEngineManager: FlutterEngineManager
     /// Creates a new instance of Omise SDK that provides interface to functionallity that SDK provides
     ///
     /// - Parameters:
@@ -44,6 +46,34 @@ public class OmiseSDK {
             vaultURL: configuration?.vaultURL
         )
 
+        /// Setup the Flutter engine and custom engine manager for presenting and communicating with the Flutter SDK.
+        ///
+        /// This section initializes the Flutter engine, creates the method channel for communication between the
+        /// native iOS SDK and Flutter, and sets up the custom handler for Flutter method calls. The manager is then
+        /// initialized to coordinate the communication and manage the Flutter engine lifecycle.
+        ///
+        /// - Parameters:
+        ///     - flutterEngine: The Flutter engine instance responsible for running the Flutter code.
+        ///     - methodChannel: The method channel used to send and receive messages between native iOS and Flutter.
+        ///     - channelHander: The custom handler for managing the method result communication callback from Flutter.
+        ///     - publicKey: The public key used for Omise API, which is passed into the manager.
+        let flutterEngine = FlutterEngine(name: OmiseFlutter.engineName)  // Initialize a new Flutter engine with a custom name
+        flutterEngine.run()  // Start the Flutter engine to prepare it for communication
+        
+        /// Create a Flutter method channel with a specified name
+        /// Use the engine's binary messenger for communication
+        let methodChannel = FlutterMethodChannel(name: OmiseFlutter.channelName,
+                                                 binaryMessenger: flutterEngine.binaryMessenger)
+        /// Instantiate the custom channel handler to handle communication via the method channel
+        let channelHander = FlutterChannelHandlerImpl(channel: methodChannel)
+        
+        flutterEngineManager = FlutterEngineManagerImpl(
+            flutterEngine: flutterEngine,
+            methodChannel: methodChannel,
+            channelHander: channelHander,
+            publicKey: publicKey
+        )
+        
         preloadCapabilityAPI()
     }
 
@@ -71,6 +101,15 @@ public class OmiseSDK {
         delegate: ChoosePaymentMethodDelegate
     ) {
         dismiss(animated: false)
+        
+        /// replace flutter screen with existing flow.
+        /// however, this doesn't have the filter functions :allowed payment method, card ayment
+        /// we will integrate that later when Flutter module supports the funciton.
+        flutterEngineManager.presentFlutterPaymentMethod(viewController: topViewController,
+                                                         amount: amount,
+                                                         currency: currency,
+                                                         delegate: delegate)
+        /*
 
         let paymentFlow = ChoosePaymentCoordinator(
             client: client,
@@ -94,12 +133,7 @@ public class OmiseSDK {
 
         let navigationController = UINavigationController(rootViewController: viewController)
         navigationController.delegate = paymentFlow
-        if #available(iOSApplicationExtension 11.0, *) {
-            navigationController.navigationBar.prefersLargeTitles = true
-        }
-
-        topViewController.present(navigationController, animated: animated, completion: nil)
-        presentedViewController = navigationController
+        */
     }
 
     /// Creates and presents modal "Credit Card Payment" controller with a given parameters
@@ -203,8 +237,14 @@ public class OmiseSDK {
     }
 
     /// Dismiss any presented UI form by OmiseSDK
+    /// Clear Flutter Screen
     public func dismiss(animated: Bool = true, completion: (() -> Void)? = nil) {
-        guard let presentedViewController = presentedViewController else { return }
+        flutterEngineManager.detachFlutterViewController(animated: animated)
+        
+        guard let presentedViewController = presentedViewController else {
+            completion?()
+            return
+        }
         presentedViewController.dismiss(animated: animated, completion: completion)
     }
 
