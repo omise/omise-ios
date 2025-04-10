c0c3e9676a9f03592f187b77f58c0d2186339202
tyler <saihanlinaung@techcombine.co>
2025-04-10 15:08:24 +0700

    update unit test for source SourceTypeViewPresentable

--- OmiseSDKTests/PaymentMethod/SelectPaymentMethodViewModelTest.swift
+++ OmiseSDKTests/PaymentMethod/SelectPaymentMethodViewModelTest.swift
@@ -8,6 +8,63 @@ class SelectPaymentMethodViewModelTest: XCTestCase {
     var mockClient: MockClient!
     var mockselectPaymentMethodDelegate: MockSelectPaymentMethodDelegate!
     
+    private let expectedIconNames: [SourceType: String] = [
+        .alipay: "Alipay",
+        .alipayCN: "AlipayCN",
+        .alipayHK: "AlipayHK",
+        .atome: "Atome",
+        .applePay: "ApplePay",
+        .barcodeAlipay: "Alipay",
+        .billPaymentTescoLotus: "Tesco",
+        .boost: "Boost",
+        .dana: "dana",
+        .duitNowOBW: "Duitnow-OBW",
+        .duitNowQR: "DuitNow-QR",
+        .eContext: "",
+        .fpx: "FPX",
+        .gcash: "gcash",
+        .grabPay: "Grab",
+        .grabPayRms: "Grab",
+        .installmentBAY: "BAY",
+        .installmentWhiteLabelBAY: "BAY",
+        .installmentBBL: "BBL",
+        .installmentWhiteLabelBBL: "BBL",
+        .installmentFirstChoice: "First Choice",
+        .installmentWhiteLabelFirstChoice: "First Choice",
+        .installmentKBank: "KBANK",
+        .installmentWhiteLabelKBank: "KBANK",
+        .installmentKTC: "KTC",
+        .installmentWhiteLabelKTC: "KTC",
+        .installmentMBB: "FPX/maybank",
+        .installmentSCB: "SCB",
+        .installmentWhiteLabelSCB: "SCB",
+        .installmentTTB: "ttb",
+        .installmentWhiteLabelTTB: "ttb",
+        .installmentUOB: "uob",
+        .installmentWhiteLabelUOB: "uob",
+        .internetBankingBAY: "BAY",
+        .internetBankingBBL: "BBL",
+        .kakaoPay: "kakaopay",
+        .maybankQRPay: "MAE-maybank",
+        .mobileBankingBAY: "KMA",
+        .mobileBankingBBL: "BBL M",
+        .mobileBankingKBank: "KPlus",
+        .mobileBankingKTB: "KTB Next",
+        .mobileBankingSCB: "SCB",
+        .ocbcDigital: "ocbc-digital",
+        .payNow: "PayNow",
+        .payPay: "PayPay",
+        .promptPay: "PromptPay",
+        .rabbitLinepay: "RabbitLinePay",
+        .shopeePay: "Shopeepay",
+        .shopeePayJumpApp: "Shopeepay",
+        .touchNGo: "touch-n-go",
+        .touchNGoAlipayPlus: "touch-n-go",
+        .trueMoneyWallet: "TrueMoney",
+        .trueMoneyJumpApp: "TrueMoney",
+        .weChat: "wechat_pay"
+    ]
+    
     override func setUp() {
         super.setUp()
         mockClient = MockClient()
@@ -42,5 +99,48 @@ class SelectPaymentMethodViewModelTest: XCTestCase {
         XCTAssertEqual(cell?.title, SourceType.trueMoneyJumpApp.localizedTitle)
         XCTAssertEqual(cell?.subtitle, SourceType.trueMoneyJumpApp.localizedSubtitle)
         XCTAssertNotNil(cell?.icon)
+    }
+    
+    func test_localizedTitleAndDescription() {
+        for source in SourceType.allCases {
+            let expectedTitle = localized("SourceType.\(source.rawValue)")
+            XCTAssertEqual(source.localizedTitle, expectedTitle)
+            XCTAssertEqual(source.description, source.localizedTitle)
+        }
+    }
+    
+    func test_iconName() {
+        for source in SourceType.allCases {
+            if let expectedIcon = expectedIconNames[source] {
+                XCTAssertEqual(source.iconName, expectedIcon)
+            } else {
+                XCTFail("No expected icon found for SourceType case: \(source)")
+            }
+        }
+    }
+    
+    func test_localizedSubtitle() {
+        for source in SourceType.allCases {
+            switch source {
+            case .alipayCN, .alipayHK, .dana, .gcash, .kakaoPay, .touchNGo:
+                let expected = localized("SourceType.alipay.footnote")
+                XCTAssertEqual(source.localizedSubtitle, expected)
+            case .grabPay:
+                let expected = localized("SourceType.grabpay.footnote")
+                XCTAssertEqual(source.localizedSubtitle, expected)
+            default:
+                XCTAssertNil(source.localizedSubtitle, "Localized subtitle for \(source) should be nil")
+            }
+        }
+    }
+    
+    func test_accessoryIcon() {
+        for source in SourceType.allCases {
+            if source.requiresAdditionalDetails {
+                XCTAssertEqual(source.accessoryIcon, Assets.Icon.next)
+            } else {
+                XCTAssertEqual(source.accessoryIcon, Assets.Icon.redirect)
+            }
+        }
     }
 }
