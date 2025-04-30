397a36f83983483044805852f524077fefbeaf1d
tyler-techcombine <saihanlinaung@techcombine.co>
2025-04-30 11:04:15 +0700

    remove testing branch

--- scripts/detect_source.sh
+++ scripts/detect_source.sh
@@ -6,8 +6,6 @@ if [[ "$GITHUB_REF" == refs/heads/main ]]; then
   echo "BUILD_SOURCE=main"
 elif [[ "$GITHUB_REF" == refs/heads/develop ]]; then
   echo "BUILD_SOURCE=develop"
-elif [[ "$GITHUB_REF" == refs/heads/feature/auto-example-app-release ]]; then
-   echo "BUILD_SOURCE=testing"
 else
   echo "BUILD_SOURCE=${GITHUB_REF#refs/tags/}"
 fi
