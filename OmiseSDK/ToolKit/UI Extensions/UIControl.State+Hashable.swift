66e71cea8d8ab543422351098ab335b3fe449690
Tyler <sai.a@opn.ooo>
2025-11-07 09:28:30 +0700

    Fix Swift 5 build by guarding compiler check (#370) (#371)

--- OmiseSDK/ToolKit/UI Extensions/UIControl.State+Hashable.swift
+++ OmiseSDK/ToolKit/UI Extensions/UIControl.State+Hashable.swift
@@ -1,8 +1,16 @@
 import UIKit
 /// ref: https://github.com/swiftlang/swift-evolution/blob/main/proposals/0364-retroactive-conformance-warning.md
-/// if we support iOS 13 and above, can remove this extension
+/// Provides Hashable conformance for older Swift toolchains.
+#if compiler(>=6)
 extension UIControl.State: @retroactive Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(rawValue)
     }
 }
+#else
+extension UIControl.State: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(rawValue)
+    }
+}
+#endif
